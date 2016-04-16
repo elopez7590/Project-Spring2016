e37f208e2115784e252aedc62f693e618dd5d1fc
OperationMech <operationmech@gmail.com>
2016-04-16 00:33:18 -0400

    Reader now works to some degree.

--- metrics_collector/src/main/java/edu/marist/metrics_collector/Reader.java
+++ metrics_collector/src/main/java/edu/marist/metrics_collector/Reader.java
@@ -6,9 +6,12 @@ package edu.marist.metrics_collector;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+
 import edu.marist.metrics_collector.database.accessor.DbAccessor;
+
 
 /**
  * Metrics Reader class
@@ -18,7 +21,7 @@ public class Reader {
    public static void main(String[] args) {
       String[] queryValues;
       ArrayList<String> queries = new ArrayList<>();
-      String query = "INSERT INTO data VALUES ('";
+      String query = "INSERT INTO metricdata VALUES ('";
       File rootDir = new File("/" + args[0]);
       File[] C = rootDir.listFiles();
       for(File F : rootDir.listFiles()) {
@@ -27,11 +30,11 @@ public class Reader {
             if(readFile(F) != null) {
                queryValues = readFile(F);
                queryAdd = query + 
-                          queryValues[0] + "','" +
-                          queryValues[1] + "','" +
-                          queryValues[3] + "','" +
-                          queryValues[22] + "','" +
-                          queryValues[21] + "')";
+                          queryValues[0]  + "','" +
+                          queryValues[1]  + "','" +
+                          getHostName()   + "','" +
+                          queryValues[3]  + "','" +
+                          Long.parseLong(queryValues[22]) + "','Now()')";
                queries.add(queryAdd);
                           
             } 
@@ -40,7 +43,7 @@ public class Reader {
       try {
          DbAccessor db = new DbAccessor();
          for(String queryOut : queries) {
-            if(!db.putData(queryOut)) {
+            if(db.putData(queryOut)) {
                System.err.println("Database busy will retry");
             }
          }
@@ -64,5 +67,17 @@ public class Reader {
          System.exit(1);
       }
       return out;
+   }
+   
+   private static String getHostName() {
+      try {
+         Runtime r = Runtime.getRuntime();
+         Process p = r.exec("uname -n");
+         BufferedReader x = new BufferedReader(new InputStreamReader(p.getInputStream()));
+         return x.readLine();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } 
+      return "Unknown";
    }
 }
