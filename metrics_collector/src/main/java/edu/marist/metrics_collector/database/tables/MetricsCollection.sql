1b0c08570f3b883cd370c0efe0407840c1f5d8dd
elopez7590 <elopez7590@gmail.com>
2016-04-18 20:33:45 -0400

    Added tables folder

---
+++ metrics_collector/src/main/java/edu/marist/metrics_collector/database/tables/MetricsCollection.sql
@@ -0,0 +1,24 @@
+﻿-- Database: "MetricsCollection"
+
+-- DROP DATABASE "MetricsCollection";
+
+--CREATE DATABASE "MetricsCollection"
+  --WITH OWNER = postgres
+  --     ENCODING = 'UTF8'-
+  --     TABLESPACE = pg_default
+  --     LC_COLLATE = 'English_United States.1252'
+  --     LC_CTYPE = 'English_United States.1252'
+  --     CONNECTION LIMIT = -1;
+
+DROP TABLE IF EXISTS metrics CASCADE;
+
+-- Metrics --
+CREATE TABLE metrics (
+  PID char(10) not null,
+  processname char(50) not null,
+  machinename char(50) not null,
+  parentPID char(10),
+  totalsize int,
+  dateofcreation date,
+  PRIMARY KEY(PID,processname,machinename)
+);
