a4228cd5749fcc79c20ee9c9df0a65bb266ab5a9
OperationMech <operationmech@gmail.com>
2016-04-21 19:13:07 -0400

    Fixed SampleJavaSql.java file.

--- metrics_collector/src/main/java/edu/marist/metrics_collector/database/accessor/SampleJavaSQL.java
+++ metrics_collector/src/main/java/edu/marist/metrics_collector/database/accessor/SampleJavaSQL.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sample.java.sql;
+package edu.marist.metrics_collector.database.accessor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,7 +20,11 @@ public class SampleJavaSQL {
     
     public SampleJavaSQL()
     {
+       try {
         db = new DbAccessor();
+       } catch (Exception e) {
+          e.printStackTrace();
+       }
     }
     
     /**
