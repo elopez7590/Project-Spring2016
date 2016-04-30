e5848d13ff92702a1d76565e0aa1b68fc4c92f53
elopez7590 <elopez7590@gmail.com>
2016-04-29 22:23:50 -0400

    Cleanup code on SampleJavaSQL

--- metrics_collector/src/main/java/edu/marist/metrics_collector/database/accessor/SampleJavaSQL.java
+++ metrics_collector/src/main/java/edu/marist/metrics_collector/database/accessor/SampleJavaSQL.java
@@ -4,8 +4,6 @@
  */
 package edu.marist.metrics_collector.database.accessor;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author EddeAnthony
@@ -23,22 +21,7 @@ public class SampleJavaSQL {
        try {
         db = new DbAccessor();
        } catch (Exception e) {
-          e.printStackTrace();
        }
-    }
-    
-    /**
-     * updateProcess method, Updates a process in the database.
-     */
-    public void updateProcess(int pid, String date)
-    {
-        try {
-           //db.putData("UPDATE MetricCollection SET WHERE PID = " + pid);      
-        } catch (Exception e) {
-           e.printStackTrace();
-           System.err.println(e.getClass().getName()+": "+e.getMessage());
-           System.exit(0);
-        }
     }
     
     /**
@@ -49,9 +32,7 @@ public class SampleJavaSQL {
         try {
            db.putData("DELETE FROM MetricCollection WHERE PID = " + pid);      
         } catch (Exception e) {
-           e.printStackTrace();
            System.err.println(e.getClass().getName()+": "+e.getMessage());
-           System.exit(0);
         }
     }
     
@@ -68,9 +49,7 @@ public class SampleJavaSQL {
                     + Integer.parseInt(data[0]) + ", '" + data[1] + "', '" + data[2] + "', " + Integer.parseInt(data[3]) 
                     + ", " + Long.parseLong(data[4]) + ", '" + data[5] + "');");
         } catch (Exception e) {
-           e.printStackTrace();
            System.err.println(e.getClass().getName()+": "+e.getMessage());
-           System.exit(0);
         }
     }
     
@@ -85,31 +64,9 @@ public class SampleJavaSQL {
         try {
             String ct = "CREATE TABLE IF NOT EXISTS " + tableName + " (PID char(10) not null, processname char(50) not null, machinename char(50) not null, parentPID char(10), totalsize bigint, dateofcreation date, PRIMARY KEY(PID,processname,machinename));";
             db.putData(ct);
-        } catch (Exception e) {
-           e.printStackTrace();
+        } catch (Exception e) {;
            System.err.println(e.getClass().getName()+": "+e.getMessage());
-           System.exit(0);
         }
-    }
-    
-    /**
-     * Send ALL data from one table to the GUI.
-     * TODO: Interface with GUI.
-     * 
-     * @param tableName String:  The table to be displayed on the GUI.
-     */
-    public void sendData(String tableName)
-    {
-        String query = "SELECT * FROM " + tableName;
-        ArrayList<String> data = db.getData(query);
-    }
-    
-    /**
-     * @param args String[]: The command line argument(s).
-     */
-    public static void main(String[] args) {
-        
-      //System.out.println("Opened database successfully");
     }
     
 }
