6908ab9f0c6344a6c2ab0de96bf5464ed23d0207
elopez7590 <elopez7590@gmail.com>
2016-05-02 22:43:42 -0400

    Additions to WebPoster- needs more work

--- metrics_collector/src/main/java/edu/marist/metrics_collector/servlet/WebPoster.java
+++ metrics_collector/src/main/java/edu/marist/metrics_collector/servlet/WebPoster.java
@@ -7,9 +7,11 @@
 package edu.marist.metrics_collector.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import edu.marist.metrics_collector.database.accessor;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import javax.script.Invocable;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,17 +19,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
+
+
 /**
  * MetricsCollector class definition and implementation.
  * 
  */
 public class WebPoster extends HttpServlet {
 	
+    public static final String javascriptFunctions = "\\Project-Spring2016\\metrics_collector\\src\\main\\java\\edu\\marist\\metrics_collector\\WebContent\\pages";
 
     /** 
      * The doGet method of the servlet. <br> 
      *  
-     * This method is called when a form has its tag value method equals to get. 
+     * This method will post each row of the database to the GUI.
      *  
      * @param request 
      *            the request send by the client to the server 
@@ -42,13 +47,7 @@ public class WebPoster extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)  
             throws ServletException, IOException {  
         
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("JavaScript");
         
-    	SampleJavaSQL sj = new SampleJavaSQL();
-        sj.getAllData();
-        //TODO: Transfer this data to Table
-        //TODO: Refresh table
         doPost(request, response);  
     }  
     
@@ -58,6 +57,19 @@ public class WebPoster extends HttpServlet {
     */
     public void refreshTable()
     {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
+        
+        try
+        {
+            engine.eval(Files.newBufferedReader(Paths.get(javascriptFunctions), StandardCharsets.UTF_8));
+            Invocable inv = (Invocable) engine;
+            String refreshScript = "function refresh()"; //may need to change this, more detail
+            inv.invokeFunction("refreshScript");
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
         
     }
 	
@@ -72,11 +84,13 @@ public class WebPoster extends HttpServlet {
    @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 				
-            String metrics = MetricsQueryDao.getAllMetrics();
-            try (PrintWriter out = response.getWriter()) {
-                out.println(metrics);
-                out.flush();
-            }
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+            SampleJavaSQL sj = new SampleJavaSQL();
+            ArrayList<String> al = sj.getAllData();
+            //TODO: Transfer this data to Table
+            refreshTable();
 	}
 
 }
